55cbfd420d8027e86af8e593c5a5e8e650bdb500
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-22 21:33:23 +0300

    add news api key

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config:
+    '''
+    general configuration parent class
+    '''
+    TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?q={}&apiKey={}'
+    SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: the parent configuratiion class with general settings
+    '''
+    DEBUG = True
