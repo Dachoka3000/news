f36c660975aab47d89d31794d7eaf5374a12416c
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-22 10:27:08 +0300

    add get source  functiion to the main index function

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .requests import get_news, get_source
 
 #views
 @app.route('/')
@@ -8,4 +9,16 @@ def index():
     '''
     view root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    general = get_source('general')
+    entertainment = get_source('entertainment')
+    business = get_source('business')
+    health = get_source('health')
+    title = 'News Today'
+    return render_template('index.html', title = title, general = general, entertainment = entertainment, business = business, health = health)
+
+@app.route('/news/business')
+def news():
+    '''
+    view news page function that returns the news details page and its data
+    '''
+    return render_template('news.html')
