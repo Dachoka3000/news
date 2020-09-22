540dadd289be5939d5d0e05d163f84207c1d4ebe
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-22 23:47:46 +0300

    add a test to check if the source class objects are set up correctly

---
+++ tests/test_source.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Source
+
+class NewsTest(unittest.TestCase):
+    '''
+    test class to test the behaviour of the source class
+    '''
+
+    def setUp(self):
+        '''
+        set up method that will run before every test
+        '''
+        self.new_source = Source("1234","abc news","https://abcnews.go.com","business","en")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news, News))
